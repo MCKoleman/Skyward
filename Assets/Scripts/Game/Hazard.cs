f0cc1c4b1bb0ea213374ae23e568131ab57aec59
MCKoleman <manu25025@gmail.com>
2022-02-20 19:16:31 -0500

    Fixed hazard code

--- Assets/Scripts/Game/Hazard.cs
+++ Assets/Scripts/Game/Hazard.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class Hazard : MonoBehaviour
 {
+    [SerializeField]
+    private int damage;
+
     // Check if colliding with the player
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter(Collision collision)
     {
         // If the player collides with a hazard, reset them to the last checkpoint
         if(collision.collider.CompareTag("Player"))
         {
-            Debug.Log("Killed player");
-            CheckpointManager.Instance.ResetToCheckpoint();
+            Character tempChar = collision.collider.GetComponent<Character>();
+            if(tempChar != null)
+            {
+                tempChar.TakeDamage(damage);
+            }
         }
     }
 }

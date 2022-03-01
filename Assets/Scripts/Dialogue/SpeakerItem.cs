ef2f7addf48b34681db033a32b7c8aeed02061de
MCKoleman <manu25025@gmail.com>
2022-03-01 17:04:19 -0500

    Added dialogue manager

---
+++ Assets/Scripts/Dialogue/SpeakerItem.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct SpeakerItem
+{
+    public GlobalVars.SpeakerType type;
+    public Sprite sprite;
+    public string name;
+    public Color bgColor;
+    public Color nameColor;
+    public Color textColor;
+}
---
+++ Assets/Scripts/ScriptableObjects/DialogueList.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DialogueList", menuName = "ScriptableObjects/DialogueList", order = 1)]
+[System.Serializable]
+public class DialogueList : ScriptableObject
+{
+    [SerializeField]
+    private List<DialogueItem> dialogue = new List<DialogueItem>();
+
+    // Returns the dialogue item at the given index
+    public DialogueItem GetDialogueAtIndex(int index)
+    {
+        return dialogue[index];
+    }
+}
